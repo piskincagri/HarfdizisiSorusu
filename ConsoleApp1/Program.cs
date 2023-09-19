a547162a6d9614fd4a2d5e903b4c3e39f9cbe2d8
cagri piskin <cagri33@hotmail.com>
2023-09-19 21:33:01 +0300

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string kelime = Console.ReadLine().ToLower();
+            if (string.IsNullOrWhiteSpace(kelime) || kelime.Length <= 1)
+            {
+                Console.WriteLine("no answer");
+                return;
+            }
+            char[] kelimeHarfleri = new char[kelime.Length];
+            int h = 0;
+            foreach (var item in kelime)
+            {
+                if (char.IsLetter(item))
+                {
+                    kelimeHarfleri[h] = item;
+                }
+                h++;
+            }
+
+
+
+
+
+
+            char degisecekIlkHarf = ' ';
+            int degisecekIlkHarfIndisi = 0;
+            char degisecekIkinciHarf = ' ';
+            int degisecekIkinciHarfIndisi = 0;
+            for (int i = kelime.Length - 1; i > 0; i--)
+            {
+                byte harfAscii = Convert.ToByte(kelimeHarfleri[i]);
+                byte harfAscii2 = Convert.ToByte(kelimeHarfleri[i - 1]);
+                if (harfAscii > harfAscii2)
+                {
+                    degisecekIlkHarf = kelimeHarfleri[i];
+                    degisecekIlkHarfIndisi = i;
+                    degisecekIkinciHarf = kelimeHarfleri[i - 1];
+                    degisecekIkinciHarfIndisi = i - 1;
+                    break;
+                }
+            }
+            if (degisecekIlkHarfIndisi != 0)
+            {
+                kelimeHarfleri[degisecekIkinciHarfIndisi] = degisecekIlkHarf;
+                kelimeHarfleri[degisecekIlkHarfIndisi] = degisecekIkinciHarf;
+                Console.WriteLine(kelimeHarfleri);
+            }
+            else
+            {
+                Console.WriteLine("no answer");
+            }
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ Harfdizisi.UI/Program.cs
@@ -0,0 +1,80 @@
+﻿ using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Harfdizisi.UI
+{
+    public  class Program
+    {
+        public static string Buyuk(string w)
+        {
+            char[] dizi = w.ToCharArray();
+
+            int i = dizi.Length - 2;
+            while (i >= 0 && dizi[i] >= dizi[i + 1])
+            {
+                i--;
+            }
+
+
+            while (i >= 0)
+            {
+                int j = dizi.Length - 1;
+                while (dizi[j] <= dizi[i])
+                {
+                    j--;
+                }
+                Degistir(dizi, i, j);
+
+                Reverse(dizi, i + 1, dizi.Length - 1);
+
+                string result = new string(dizi);
+                if (!result.Equals(w))
+                {
+                    return result;
+                }
+
+                i--;
+            }
+
+            return "no answer";
+        }
+
+        private static void Degistir(char[] dizi, int i, int j)
+        {
+            char temp = dizi[i];
+            dizi[i] = dizi[j];
+            dizi[j] = temp;
+        }
+
+        private static void Reverse(char[] dizi, int start, int end)
+        {
+            while (start < end)
+            {
+                Degistir(dizi, start, end);
+                start++;
+                end--;
+            }
+        }
+
+        public static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.Write("Bir kelime girin (Programdan çıkmak için 'exit' yazın): ");
+                string kelime = Console.ReadLine();
+
+                if (kelime.ToLower() == "exit")
+                {
+                    break;
+                }
+
+                string sonuc = Buyuk(kelime);
+
+                Console.WriteLine("Sonuç: " + sonuc);
+            }
+        }
+    }
+}
